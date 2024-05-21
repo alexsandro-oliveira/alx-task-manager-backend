5dcd6e56b3b8ad220461e3eff5386c5a183ac027
Alexs <ale.osantos@hotmail.com>
2024-05-21 15:08:22 +0100

    feat: add route to get a task by id

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ app.get("/tasks", async (req, res) => {
   }
 });
 
+app.get("/tasks/:id"),
+  async (req, res) => {
+    try {
+      const taskId = req.params.id;
+
+      const task = await TaskModel.findById(taskId);
+
+      if (!task) {
+        return res.status(404).send("Essa tarefa não foi encontrada.");
+      }
+
+      return res.status(200).send(task);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
+
 app.post("/tasks", async (req, res) => {
   try {
     const newTask = new TaskModel(req.body); // criar uma nova task
@@ -38,7 +55,7 @@ app.delete("/tasks/:id", async (req, res) => {
     const taskToDelete = await TaskModel.findById(taskId);
 
     if (!taskToDelete) {
-      return res.status(500).send("Essa tarefa não foi encontrada.");
+      return res.status(404).send("Essa tarefa não foi encontrada.");
     }
 
     const deleteTask = await TaskModel.findByIdAndDelete(taskId);
