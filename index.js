2cb682b52be318e814782cfa710d86dace9111ae
Alexs <ale.osantos@hotmail.com>
2024-05-21 19:46:07 +0100

    feat: add router file to task entity

--- index.js
+++ index.js
@@ -1,98 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const TaskRouter = require("./src/routes/task.routes");
 
 const connectToDatabase = require("./src/database/mongoose.database");
-const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
+
 const app = express();
 app.use(express.json()); //middleware
 
 connectToDatabase();
 
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await TaskModel.find({});
-    res.status(200).send(tasks);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.get("/tasks/:id"),
-  async (req, res) => {
-    try {
-      const taskId = req.params.id;
-
-      const task = await TaskModel.findById(taskId);
-
-      if (!task) {
-        return res.status(404).send("Essa tarefa não foi encontrada.");
-      }
-
-      return res.status(200).send(task);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  };
-
-app.post("/tasks", async (req, res) => {
-  try {
-    const newTask = new TaskModel(req.body); // criar uma nova task
-
-    await newTask.save(); // salvar no banco.
-
-    res.status(201).send(newTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const taskData = req.body;
-
-    const taskToUpdate = await TaskModel.findById(taskId);
-
-    const allowedUpdates = ["isCompleted"];
-    const requestedUpdates = Object.keys(taskData);
-
-    for (update of requestedUpdates) {
-      if (allowedUpdates.includes(update)) {
-        taskToUpdate[update] = taskData[update];
-      } else {
-        return res
-          .status(500)
-          .send("Um ou mais campos inseridos não são editáveis.");
-      }
-    }
-
-    await taskToUpdate.save();
-    return res.status(200).send(taskToUpdate);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-
-    const taskToDelete = await TaskModel.findById(taskId);
-
-    if (!taskToDelete) {
-      return res.status(404).send("Essa tarefa não foi encontrada.");
-    }
-
-    const deleteTask = await TaskModel.findByIdAndDelete(taskId);
-
-    res.status(200).send(deleteTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+app.use("/tasks", TaskRouter);
 
 const PORT = 8000;
-
-app.listen(PORT, () => console.log(`Listening on port: ${8000}!`));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}!`));
