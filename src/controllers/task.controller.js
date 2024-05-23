e46a9e9f73c17b7e9387afdbf9fff801a343dd4e
Alexs <ale.osantos@hotmail.com>
2024-05-23 22:34:45 +0100

    feat: move delete task endpoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -69,6 +69,24 @@ class TaskController {
       this.res.status(500).send(error.message);
     }
   }
+
+  async delete() {
+    try {
+      const taskId = this.req.params.id;
+
+      const taskToDelete = await TaskModel.findById(taskId);
+
+      if (!taskToDelete) {
+        return this.res.status(404).send("Essa tarefa não foi encontrada.");
+      }
+
+      const deleteTask = await TaskModel.findByIdAndDelete(taskId);
+
+      this.res.status(200).send(deleteTask);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
 }
 
 module.exports = TaskController;
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -22,21 +22,7 @@ router.patch("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-
-    const taskToDelete = await TaskModel.findById(taskId);
-
-    if (!taskToDelete) {
-      return res.status(404).send("Essa tarefa não foi encontrada.");
-    }
-
-    const deleteTask = await TaskModel.findByIdAndDelete(taskId);
-
-    res.status(200).send(deleteTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).delete();
 });
 
 module.exports = router;
