e772a0f7dc1134645635c1038152cb73df140e78
Alexs <ale.osantos@hotmail.com>
2024-05-23 22:19:24 +0100

    feat: move get tasks by id endpoint to a controller

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -9,22 +9,9 @@ router.get("/", async (req, res) => {
   return new TaskController(req, res).getTask();
 });
 
-router.get("/:id"),
-  async (req, res) => {
-    try {
-      const taskId = req.params.id;
-
-      const task = await TaskModel.findById(taskId);
-
-      if (!task) {
-        return res.status(404).send("Essa tarefa não foi encontrada.");
-      }
-
-      return res.status(200).send(task);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  };
+router.get("/:id", async (req, res) => {
+  return new TaskController(req, res).getById();
+});
 
 router.post("/", async (req, res) => {
   try {
