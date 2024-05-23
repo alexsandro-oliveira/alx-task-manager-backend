e3580448557c1e251c0a8eb40ea1d9ff8ede3dad
Alexs <ale.osantos@hotmail.com>
2024-05-23 23:23:18 +0100

    refactor: move not fond error to a separate file

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,4 +1,5 @@
 const TaskModel = require("../models/task.model");
+const { notFoundError } = require("../errors/mongodb.erros");
 
 class TaskController {
   constructor(req, res) {
@@ -22,7 +23,7 @@ class TaskController {
       const task = await TaskModel.findById(taskId);
 
       if (!task) {
-        return this.res.status(404).send("Essa tarefa não foi encontrada.");
+        return notFoundError(this.res);
       }
 
       return this.res.status(200).send(task);
@@ -49,6 +50,10 @@ class TaskController {
       const taskData = this.req.body;
 
       const taskToUpdate = await TaskModel.findById(taskId);
+
+      if (!taskToUpdate) {
+        return notFoundError(this.res);
+      }
 
       const allowedUpdates = ["isCompleted"];
       const requestedUpdates = Object.keys(taskData);
@@ -77,7 +82,7 @@ class TaskController {
       const taskToDelete = await TaskModel.findById(taskId);
 
       if (!taskToDelete) {
-        return this.res.status(404).send("Essa tarefa não foi encontrada.");
+        return notFoundError(this.res);
       }
 
       const deleteTask = await TaskModel.findByIdAndDelete(taskId);
